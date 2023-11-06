7f142cf8fe1484ec09cfe446f97ee502e1d40ba8
jpope6 <imthepope@csu.fullerton.edu>
2023-11-06 15:04:59 -0800

    Added backend url

--- frontend/src/context/BackendUrlContext.js
+++ frontend/src/context/BackendUrlContext.js
@@ -3,7 +3,11 @@ import { useState, createContext } from "react";
 export const BackendUrlContext = createContext();
 
 export function BackendUrlProvider({ children }) {
-	const [backendUrl, setBackendUrl] = useState("http://localhost:5000");
+	const [backendUrl, setBackendUrl] = useState(
+		process.env.NODE_ENV === "production"
+			? "https://cloudy-crow-sunbonnet.cyclic.app"
+			: "http://localhost:5000"
+	);
 
 	return (
 		<BackendUrlContext.Provider value={backendUrl}>
