8de71e893e595e9ac33a6f96a0d28e7d9f8fb677
jpope6 <imthepope@csu.fullerton.edu>
2024-05-25 10:59:09 -0700

    Update url

--- frontend/src/context/BackendUrlContext.js
+++ frontend/src/context/BackendUrlContext.js
@@ -3,15 +3,15 @@ import { useState, createContext } from "react";
 export const BackendUrlContext = createContext();
 
 export function BackendUrlProvider({ children }) {
-	const [backendUrl, setBackendUrl] = useState(
-		process.env.NODE_ENV === "production"
-			? "https://cloudy-crow-sunbonnet.cyclic.app"
-			: "http://localhost:5000"
-	);
+  const [backendUrl, setBackendUrl] = useState(
+    process.env.NODE_ENV === "production"
+      ? "https://moneysync.onrender.com"
+      : "http://localhost:5000"
+  );
 
-	return (
-		<BackendUrlContext.Provider value={backendUrl}>
-			{children}
-		</BackendUrlContext.Provider>
-	);
+  return (
+    <BackendUrlContext.Provider value={backendUrl}>
+      {children}
+    </BackendUrlContext.Provider>
+  );
 }
